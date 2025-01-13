e6846b9f67e86da4a501452ec5ec78c299505ff4
Daniel Chalmers <daniel.chalmers@outlook.com>
2025-01-12 21:17:49 -0600

    don't render the current camera as a mini camera

--- TeslaCam/FFmpegHandler.cs
+++ TeslaCam/FFmpegHandler.cs
@@ -1,6 +1,5 @@
 ﻿using System.IO;
 using CliWrap;
-using CliWrap.Builders;
 using Serilog;
 using TeslaCam.Data;
 using Unosquare.FFME;
@@ -38,7 +37,7 @@ public class FFmpegHandler
     /// <param name="chunk">The CamChunk containing the files of camera videos.</param>
     /// <param name="outputFile">The output video file path.</param>
     /// <returns>Task representing the overlay process.</returns>
-    private async Task OverlayCamerasAsync(CamChunk chunk, string outputFile)
+    private async Task OverlayCamerasAsync(CamChunk chunk, string outputFile, string selectedCamera = "front")
     {
         if (chunk == null || !chunk.Files.Any())
             throw new ArgumentException("Chunk cannot be null or empty.", nameof(chunk));
@@ -52,31 +51,31 @@ public class FFmpegHandler
         var cameraPadding = 30;
 
         var filterComplex = $@"
-            [1:v]scale={resolution}[front_scaled];
-            [front_scaled]drawtext=fontfile={fontPath}:text='Front':x=5:y=h-25:fontsize=20:fontcolor=white[front_labeled];
-            [2:v]scale={resolution}[back_scaled];
-            [back_scaled]drawtext=fontfile={fontPath}:text='Back':x=5:y=h-25:fontsize=20:fontcolor=white[back_labeled];
-            [3:v]scale={resolution}[left_scaled];
-            [left_scaled]drawtext=fontfile={fontPath}:text='Left':x=5:y=h-25:fontsize=20:fontcolor=white[left_labeled];
-            [4:v]scale={resolution}[right_scaled];
-            [right_scaled]drawtext=fontfile={fontPath}:text='Right':x=5:y=h-25:fontsize=20:fontcolor=white[right_labeled];
+            [1:v]scale={resolution}[top_left_scaled];
+            [top_left_scaled]drawtext=fontfile={fontPath}:text='Front':x=5:y=h-25:fontsize=20:fontcolor=white[top_left_labeled];
+            [2:v]scale={resolution}[top_right_scaled];
+            [top_right_scaled]drawtext=fontfile={fontPath}:text='Back':x=5:y=h-25:fontsize=20:fontcolor=white[top_right_labeled];
+            [3:v]scale={resolution}[bottom_left_scaled];
+            [bottom_left_scaled]drawtext=fontfile={fontPath}:text='Left':x=5:y=h-25:fontsize=20:fontcolor=white[bottom_left_labeled];
+            [4:v]scale={resolution}[bottom_right_scaled];
+            [bottom_right_scaled]drawtext=fontfile={fontPath}:text='Right':x=5:y=h-25:fontsize=20:fontcolor=white[bottom_right_labeled];
 
-            [0:v][front_labeled]overlay={cameraPadding}:{cameraPadding}:shortest=1[front_overlay];
-            [front_overlay][back_labeled]overlay=W-{resolution.Split('x')[0]}-{cameraPadding}:{cameraPadding}:shortest=1[back_overlay];
-            [back_overlay][left_labeled]overlay={cameraPadding}:H-{resolution.Split('x')[1]}-{cameraPadding}:shortest=1[left_overlay];
-            [left_overlay][right_labeled]overlay=W-{resolution.Split('x')[0]}-{cameraPadding}:H-{resolution.Split('x')[1]}-{cameraPadding}[output]";
+            [0:v][top_left_labeled]overlay={cameraPadding}:{cameraPadding}:shortest=1[top_left_overlay];
+            [top_left_overlay][top_right_labeled]overlay=W-{resolution.Split('x')[0]}-{cameraPadding}:{cameraPadding}:shortest=1[top_right_overlay];
+            [top_right_overlay][bottom_left_labeled]overlay={cameraPadding}:H-{resolution.Split('x')[1]}-{cameraPadding}:shortest=1[bottom_left_overlay];
+            [bottom_left_overlay][bottom_right_labeled]overlay=W-{resolution.Split('x')[0]}-{cameraPadding}:H-{resolution.Split('x')[1]}-{cameraPadding}[output]";
 
 
         List<string> args = [
             "-y", // Overwrite existing files
-            "-i", $@"{chunk.Files["front"].FullPath}",
+            "-i", $@"{chunk.Files[selectedCamera].FullPath}",
         ];
 
         void AddCam(string name)
         {
             var file = chunk.Files.GetValueOrDefault(name);
 
-            if (file is null)
+            if (file is null || file.Camera == selectedCamera)
             {
                 args.Add("-f");
                 args.Add("lavfi");
@@ -101,7 +100,9 @@ public class FFmpegHandler
             "-c:v", "libx264",
             "-preset", "ultrafast",
             "-movflags", "+faststart",
+#if DEBUG
             "-report", // Debugging log
+#endif
             outputFile
         ]);
 
